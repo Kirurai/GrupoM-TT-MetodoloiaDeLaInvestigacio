ac62e0a424165579d0d0286fd4fe2dc2c694926f
bdefilippi <bruno.de@alumnos.frm.utn.edu.ar>
2020-05-13 17:44:05 -0300

    Actualizacion del codigo

---
+++ TP5/ConstanteDeKaprekar.py
@@ -0,0 +1,21 @@
+def  rutinaKaprekar(casos, numeros):
+
+
+def ingresaNumero(casos):   #funcion para ingresar los numeros a comprobar
+
+    numeros=[]  #vector de numeros
+
+    while len(numeros)<casos:   #mientras falten numeros
+
+        numNuevo = input("Ingrese un numero de 4 cifras, que tenga al menos 2 diferentes: ") #pide un numero
+
+        #este bloque revisa que el numero sea de 4 cifras y que cumpla con que al menos 2 cifras sean diferentes
+        if numNuevo<9999:
+
+        else:
+            print("Número erroneo.")
+
+def revisar
+
+
+casosDePrueba = input("Ingrese el numero de casos de prueba: ")
