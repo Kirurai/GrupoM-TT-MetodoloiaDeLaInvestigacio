926b4adf7e0298f5c8684124927668f6a1bfad5b
Kirurai <kd.maurii@gmail.com>
2020-05-14 00:42:27 -0300

    Arregla la versión de archivo que se subió

--- TP5 - Miño/Kaprekar.py
+++ TP5 - Miño/Kaprekar.py
@@ -7,12 +7,11 @@ while True:
 if numero == "6174":
     "Ha ingresado la constante de Kaprekar. Felicidades lo resolvió en 0 iteraciones"
 
-for x in range(0, 4 - len(numero)):
+while len(numero) != 4:
     numero += "0"
 
 k = 0
 # Una vez llegado a la cte de Kaprekar o superado las 7 iteraciones. El while finalizará
-print("{} es el tipo de numero".format(type(numero)))
 while (numero != "6174") and (k < 8):
     ascendente = []
     descendente = []
@@ -20,12 +19,10 @@ while (numero != "6174") and (k < 8):
         ascendente.append(numero[x])
         descendente.append(numero[x])
 
-    sorted(ascendente)
-    sorted(descendente, reverse=True)
-
+    ascendente = sorted(ascendente)
+    descendente = sorted(descendente, reverse=True)
     k += 1
-    numero = (abs(int("".join(ascendente)) - int("".join(descendente))))
-    print(numero)
+    numero = str((abs(int("".join(ascendente)) - int("".join(descendente)))))
 
 if k == 8:
     print("Exceso de iteraciones. Ha ocurrido un error al validar los datos. Contactesé con su diosito de turno")
